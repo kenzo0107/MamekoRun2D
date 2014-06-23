4391f9d021dde09bdc2c2a15dddb3dd70850babc
kenzo <kenzo.tanaka0107@gmail.com>
2014-06-24 00:11:57 +0900

    ・会話機能（暫定組み込み）

---
+++ Assets/Scripts/PlayRunningGame/ComversationManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ComversationManager : MonoBehaviour {
+
+	/// <summary>
+	/// 次の話へ.
+	/// </summary>
+	private void NextTalk( ) {
+		Debug.Log( "NextTalk" );
+	}
+}
